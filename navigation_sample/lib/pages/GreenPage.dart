0360f84325b6e8aac6a3087d8c3d386d192d3ca3
y4gamilight <thanhlt.ios.36@gmail.com>
2020-04-01 00:07:58 +0700

    -Update direct navigation

---
+++ navigation_sample/lib/pages/GreenPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GreenPage extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      body: Center( 
+        child: Container(
+      decoration: BoxDecoration(
+        color: Colors.green,
+      ),
+    ),
+      ),
+    );
+  }
+}
